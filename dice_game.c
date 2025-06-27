dbe2a6644897debd511286f6448c246646647702
kawamonn91 <kawamonn91@gmail.com>
2025-06-27 12:20:56 +0900

    Won/lost message added

--- dice_game.c
+++ dice_game.c
@@ -26,5 +26,12 @@ int main(void) {
     printf("Die 1: %d\n", die1);
     printf("Die 2: %d\n", die2);
     printf("Total value: %d\n", total);
+    if (total > 7) {
+        printf("You won!\n");
+    } else {
+        printf("You lost!\n");
+    }
+
+
     return 0;
 }
